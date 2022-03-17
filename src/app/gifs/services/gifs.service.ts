14958be10f2c1b3d741bf231b668376d2412b4e9
Leonardo Garcia <LegazaGZ@outlook.com>
2022-03-17 11:43:57 -0600

    Manejo de HIstorial en localStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -16,7 +16,14 @@ export class GifsService {
     return[...this._historial];
   }
   
-  constructor( private http:HttpClient ){}
+  constructor( private http:HttpClient ){
+
+    this._historial = JSON.parse( localStorage.getItem('historial')! ) || [];
+    
+    // if(localStorage.getItem('historial')){
+    //   this._historial=JSON.parse( localStorage.getItem('historial')! ) ;
+    // }
+  }
 
   buscarGifs( query:string){
 
@@ -26,9 +33,11 @@ export class GifsService {
 
     if( !this._historial.includes(query) ){
       this._historial.unshift(query);
+      this._historial = this._historial.splice(0, 10);
+
+      localStorage.setItem('historial', JSON.stringify(this._historial));
     }
 
-    this._historial = this._historial.splice(0, 10);
 
     this.http.get<SearchGifsResponse>(`http://api.giphy.com/v1/gifs/search?api_key=uMziCBJgBdS67CdIxHBu1Pow7hBACSnH&q=${query}&limit=10`)
       .subscribe( (resp) => {
